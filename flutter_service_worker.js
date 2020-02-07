1208522a8b9fe34c6bc148b893fd409b49400422
ErgoFriend <ErgoFriend@users.noreply.github.com>
2020-02-07 17:28:03 +0000

    Automated deployment: Fri Feb  7 17:28:03 UTC 2020 e3d64dc1d56107905473411970a301f7573a4629

---
+++ flutter_service_worker.js
@@ -0,0 +1,41 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/index.html": "19f999aa97c23813ec4aabfbde517561",
+"/manifest.json": "6b5993b4ec10de92db95e83295cab899",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"/assets/LICENSE": "bef928eda7dc467ddf26bd74087f1c24",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/main.dart.js": "2ca5c087da793c96bccdb2e7d4a65f04",
+"/favicon.png": "5dcef449791fa27946b3d35ad8803796"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request, {
+          credentials: 'include'
+        });
+      })
+  );
+});
